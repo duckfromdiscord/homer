2b8b3a4e0f09196156c666defbdde0256ea35e52
duckfromdiscord <113956421+duckfromdiscord@users.noreply.github.com>
2023-03-28 11:06:06 -0400

    add midpoint feature

--- src/app.rs
+++ src/app.rs
@@ -13,7 +13,7 @@ impl HomerApp {
     }
 
     fn plot(&self, ui: &mut egui::Ui) -> egui::Response {
-        use egui::plot::{Line, PlotPoints};
+        use egui::plot::{Line, PlotPoints, Points};
         use meval::*;
         use std::str::FromStr;
 
@@ -27,16 +27,24 @@ impl HomerApp {
             Err(_) => -1.0,
         };
 
-        let n = 128;
+        let n: u8 = match self.n.parse() {
+            Ok(x) => x,
+            Err(_) => 3,
+        };
+
+        let resolution = 128;
+
+        let expr = match Expr::from_str(&self.expression) {
+            Ok(x) => x,
+            Err(_) => Expr::from_str("0").unwrap(),
+        };
+
+        // Function Line
         let line = Line::new(
-            (0..=n)
+            (0..=resolution)
                 .map(|i| {
 
-                    let x = egui::remap(i as f64, 0.0..=n as f64, lower..=upper);
-                    let expr = match Expr::from_str(&self.expression) {
-                        Ok(x) => x,
-                        Err(_) => Expr::from_str("0").unwrap(),
-                    };
+                    let x = egui::remap(i as f64, 0.0..=resolution as f64, lower..=upper);
                     let mut ctx = Context::new();
                     ctx.var("x", x);
                     [x, expr.eval_with_context(ctx).unwrap()]
@@ -44,6 +52,23 @@ impl HomerApp {
                 })
                 .collect::<PlotPoints>(),
         );
+
+        // Midpoint
+
+        let mp_dx: f64 = (upper-lower)/(n as f64);
+        let mut mp_subints: Vec<[f64; 2]> = vec![];
+        let mut mp_idx: f64 = lower;
+
+        for i in 0..n {
+            mp_idx = (lower) + (i as f64 * mp_dx);
+            let x = ( (mp_idx) + (mp_idx + mp_dx) ) / 2.0;
+            let mut ctx = Context::new();
+            ctx.var("x", x);
+            mp_subints.push( [x, expr.eval_with_context(ctx).unwrap()] );
+        }
+        
+        let mp_plotpoints: PlotPoints = PlotPoints::new(mp_subints);
+
         egui::plot::Plot::new("plot")
             .show_axes([true, true])
             .allow_drag(true)
@@ -54,7 +79,10 @@ impl HomerApp {
             .width(600.0)
             .height(400.0)
             .data_aspect(1.0)
-            .show(ui, |plot_ui| plot_ui.line(line))
+            .show(ui, |plot_ui| {
+                plot_ui.line(line);
+                plot_ui.points(Points::new(mp_plotpoints));
+            })
             .response
     }
 
@@ -90,6 +118,12 @@ impl eframe::App for HomerApp {
                 ui.label("n = ");
                 ui.add_sized( [20.0, 9.0], egui::TextEdit::singleline(&mut self.n) );
             });
+
+
+            self.expression = "x^2".to_string();
+            self.upper = "3".to_string();
+            self.lower = "0".to_string();
+            self.n = "4".to_string();
 
         });
 
